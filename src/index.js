b8418216f238a79aefd6e4f126f01c48e1af050e
bospur <bospobob@yandex.ru>
2022-09-15 13:40:32 +0500

    mongoDB

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-const express = require('express');
-const { ApolloServer, gql } = require('apollo-server-express');
+const express = require("express");
+const { ApolloServer, gql } = require("apollo-server-express");
+require('dotenv').config();
+
+const db = require('./db');
+const models = require('./models');
+
 const port = process.env.PORT || 4000;
+const DB_HOST = process.env.DB_HOST;
 
 let notes = [
-    {id : '1', content: 'This is a note', author: 'Adam Scott'},
-    {id : '2', content: 'This is another a note', author: 'Mary Po'},
-    {id : '3', content: 'O hey look, another note', author: 'Ivan Sem'},
+    { id: "1", content: "This is a note", author: "Adam Scott" },
+    { id: "2", content: "This is another a note", author: "Mary Po" },
+    { id: "3", content: "O hey look, another note", author: "Ivan Sem" },
 ];
 
 const typeDefs = gql`
@@ -17,7 +23,7 @@ const typeDefs = gql`
     type Query {
         hello: String!
         notes: [Note!]!
-        note(id : ID!): Note!
+        note(id: ID!): Note!
     }
     type Mutation {
         newNote(content: String!): Note!
@@ -26,38 +32,40 @@ const typeDefs = gql`
 
 const resolvers = {
     Query: {
-        hello: () => 'Hello world',
-        notes: () => notes,
-        note: (parent, args) => {
-            return notes.find(note => note.id === args.id);
-        }
+        hello: () => "Hello world",
+        notes: async () => {
+            return await models.Note.find();
+        },
+        note: async (parent, args) => {
+            return await models.Note.findById(args.id);
+        },
     },
     Mutation: {
-        newNote: (parent, args) => {
-            const noteValue = {
-                id: String(notes.length + 1),
+        newNote: async (parent, args) => {
+            return await models.Note.create({
                 content: args.content,
                 author: 'Adam Scott'
-            };
-            notes.push(noteValue);
-            return noteValue;
-        }
-    }
+            })
+        },
+    },
 };
 
+
 const app = express();
+
+db.connect(DB_HOST);
 
 async function startServer() {
     const apolloServer = new ApolloServer({
         typeDefs,
         resolvers,
     });
-    
+
     await apolloServer.start();
 
-    apolloServer.applyMiddleware({ app, path: '/' });
+    apolloServer.applyMiddleware({ app, path: "/api" });
 
-    app.get('/', (req, res) => res.send('Hello World!!!'));
+    app.get("/", (req, res) => res.send("Hello World!!!"));
     app.listen(port, () => {
         console.log(
             `GraphQL server running at http://localhost:${port}${apolloServer.graphqlPath}`
