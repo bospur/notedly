1cd91908fb4e8100d3c00e011bd62f8c39afa7f8
bospur <bospobob@yandex.ru>
2022-09-16 12:56:30 +0500

    grapQL iso Datetime

--- src/index.js
+++ src/index.js
@@ -1,55 +1,14 @@
 const express = require("express");
-const { ApolloServer, gql } = require("apollo-server-express");
+const { ApolloServer } = require("apollo-server-express");
 require('dotenv').config();
 
 const db = require('./db');
 const models = require('./models');
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
 
 const port = process.env.PORT || 4000;
 const DB_HOST = process.env.DB_HOST;
-
-let notes = [
-    { id: "1", content: "This is a note", author: "Adam Scott" },
-    { id: "2", content: "This is another a note", author: "Mary Po" },
-    { id: "3", content: "O hey look, another note", author: "Ivan Sem" },
-];
-
-const typeDefs = gql`
-    type Note {
-        id: ID!
-        content: String!
-        author: String!
-    }
-    type Query {
-        hello: String!
-        notes: [Note!]!
-        note(id: ID!): Note!
-    }
-    type Mutation {
-        newNote(content: String!): Note!
-    }
-`;
-
-const resolvers = {
-    Query: {
-        hello: () => "Hello world",
-        notes: async () => {
-            return await models.Note.find();
-        },
-        note: async (parent, args) => {
-            return await models.Note.findById(args.id);
-        },
-    },
-    Mutation: {
-        newNote: async (parent, args) => {
-            return await models.Note.create({
-                content: args.content,
-                author: 'Adam Scott'
-            })
-        },
-    },
-};
-
 
 const app = express();
 
@@ -59,13 +18,15 @@ async function startServer() {
     const apolloServer = new ApolloServer({
         typeDefs,
         resolvers,
+        context: () => {
+            return { models };
+        }
     });
 
     await apolloServer.start();
 
     apolloServer.applyMiddleware({ app, path: "/api" });
 
-    app.get("/", (req, res) => res.send("Hello World!!!"));
     app.listen(port, () => {
         console.log(
             `GraphQL server running at http://localhost:${port}${apolloServer.graphqlPath}`
---
+++ src/resolvers/mutation.js
@@ -0,0 +1,28 @@
+module.exports = {
+    newNote: async (parent, args, { models }) => {
+        return await models.Note.create({
+            content: args.content,
+            author: 'Adam Scott'
+        });
+    },
+    deleteNote: async (parent, { id }, { models }) => {
+        try {
+            await models.Note.findOneAndRemove({ _id: id })
+            return true;
+        }
+        catch (err) {
+
+        }
+    },
+    updateNote: async (parent, { id, content }, { models }) => {
+        return await models.Note.findOneAndUpdate(
+            { _id: id },
+            {
+                $set: {
+                    content
+                }
+            },
+            { new: true }
+        )
+    }
+}
